3070062edcbebfb14b272ea06b5bbc0858cbecc0
HexinHexin <32285422+Lhx94As@users.noreply.github.com>
2021-10-12 21:32:16 +0800

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import random
+
 import scipy
 import shutil
 import numpy as np
@@ -29,6 +31,15 @@ def collate_fn_atten(batch):
     data = rnn_utils.pad_sequence(seq, batch_first=True, padding_value=0)
     labels = torch.LongTensor(labels)
     return data, labels, seq_length
+
+def collate_fn_atten_kd(batch):
+    batch.sort(key=lambda x: x[2], reverse=True)
+    seq, short_seq, labels, seq_length = zip(*batch)
+    data = rnn_utils.pad_sequence(seq, batch_first=True, padding_value=0)
+    data_short = rnn_utils.pad_sequence(short_seq, batch_first=True, padding_value=0)
+    labels = torch.LongTensor(labels)
+    return data, data_short, labels, seq_length
+
 
 
 def collate_fn_cnn_atten(batch):
@@ -68,18 +79,52 @@ class RawFeatures(data.Dataset):
         return len(self.label_list)
 
 
+class KD_RawFeatures(data.Dataset):
+    def __init__(self, txt_path):
+        with open(txt_path, 'r') as f:
+            lines = f.readlines()
+            self.feature_list = [i.split()[0] for i in lines]
+            self.label_list = [i.split()[1] for i in lines]
+            self.seq_len_list = [i.split()[2] for i in lines]
+
+    def __getitem__(self, index):
+        feature_path = self.feature_list[index]
+        feature = torch.from_numpy(np.load(feature_path, allow_pickle=True))
+        short_feature = torch.from_numpy(np.load(feature_path, allow_pickle=True)[:5,:])
+        label = int(self.label_list[index])
+        seq_len = int(self.seq_len_list[index])
+        return feature, short_feature, label, seq_len
+
+    def __len__(self):
+        return len(self.label_list)
+
+
+
 def get_atten_mask(seq_lens, batch_size):
     max_len = seq_lens[0]
-    # assert len(seq_lens) != batch_size
     atten_mask = torch.ones([batch_size, max_len, max_len])
     for i in range(batch_size):
         length = seq_lens[i]
-        atten_mask[i, :length,:length] = 0
+        atten_mask[i, :length, :length] = 0
     return atten_mask.bool()
 
-def get_atten_mask_student(seq_lens, batch_size, mask_type='fix', utt_len=15):
-    max_len = seq_lens[0]
-    atten_mask = torch.ones([batch_size, max_len, max_len])
+def get_atten_mask_student(seq_lens, batch_size, mask_type='fix', win_len=15):
+  max_len = seq_lens[0]
+  atten_mask = torch.ones([batch_size, max_len, max_len])
+  if mask_type == 'fix':
     for i in range(batch_size):
-        atten_mask[i, 0:utt_len, 0:utt_len] = 0
-    return atten_mask.bool()
+      if seq_len>win_len:
+        atten_mask[i, 0:win_len, 0:win_len] = 0
+      else:
+        atten_mask[i, :seq_len, :seq_len] = 0
+  elif mask_type == 'random':
+    for i in range(batch_size):
+      seq_len = seq_lens[i]
+      if seq_len>win_len:
+          rest_len = seq_len - win_len
+          start = random.randint(0, rest_len)
+          end = start + win_len
+          atten_mask[i, start:end, start:end] = 0
+      else:
+          atten_mask[i, :seq_len, :seq_len] = 0
+  return atten_mask.bool()
